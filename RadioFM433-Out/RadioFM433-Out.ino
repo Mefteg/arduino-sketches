b26736780c666027a97d01b4a80853a8db519eae
Mefteg <gimenez.tom@gmail.com>
2015-04-19 20:35:33 -0400

    Delay the transmission

--- RadioFM433-Out/RadioFM433-Out.ino
+++ RadioFM433-Out/RadioFM433-Out.ino
@@ -1,12 +1,28 @@
+#define LED 13
+
+unsigned int send = 0;
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+  
   Serial.begin(1200); // Hardware supports up to 2400, but 1200 gives longer range
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  Serial.write(15);
+  if (send == 0)
+  {
+    digitalWrite(LED, LOW);
+    
+    send = 1;
+  }
+  else
+  {
+    digitalWrite(LED, HIGH);
+    Serial.write(15);
+    
+    send = 0;
+  }
 
   delay(1000);
 }
