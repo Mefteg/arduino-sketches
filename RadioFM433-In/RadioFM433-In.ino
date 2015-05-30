e6d9fae6b687df3c656e4874f77f46f4678915ed
Mefteg <gimenez.tom@gmail.com>
2015-05-30 18:28:02 -0400

    Reveiver is working since using VirtualWire lib

--- RadioFM433-In/RadioFM433-In.ino
+++ RadioFM433-In/RadioFM433-In.ino
@@ -1,13 +1,45 @@
+#include <VirtualWire.h>
+
+#define LED 13
+#define RECEIVER 12
+#define TRANSMITER 11
+#define TRANSMIT_EN_PIN 3
+
+#define SERIAL_SPEED 2000
+
 void setup() {
+  delay(1000);
   // put your setup code here, to run once:
-  Serial.begin(1200);  // Hardware supports up to 2400, but 1200 gives longer range
+  pinMode(LED, OUTPUT);
+
+  vw_set_tx_pin(TRANSMITER);
+  vw_set_rx_pin(RECEIVER);
+  vw_set_ptt_pin(TRANSMIT_EN_PIN);
+  
+  vw_set_ptt_inverted(true); // Configure the "push to talk" polarity. Required for DR3100 (-> TODO : TEST IF NEEDED)
+  
+  vw_setup(SERIAL_SPEED);
+  
+  vw_rx_start();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  while(Serial.available() == 0); // Wait until something is avalaible
-  byte b = Serial.read();
-  Serial.write(value);
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
   
-  delay(10);
+  if (vw_get_message(buf, &buflen)) // Non-blocking
+  {
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);
+    
+    if(buf[0] == '1')
+    {   
+      digitalWrite(LED, HIGH);
+      delay(2000);
+      digitalWrite(LED, LOW);
+    }  
+  }
+  
+  delay(1);
 }
